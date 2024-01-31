0e0b94159819b90b8af3d76288b40de82801ae17
Dan <danielfitzgerald@zoho.com>
2024-01-31 13:38:35 +0000

    Add files via upload

---
+++ Autopilot.ps1
@@ -0,0 +1,5 @@
+& '.\Prereqs\WiFi.bat'
+Start-Sleep -Seconds 5
+& '.\Prereqs\AutoImportHash.exe'
+Start-Sleep -Seconds 180
+systemreset -factoryreset
---
+++ WiFi.bat
@@ -0,0 +1,2 @@
+Netsh WLAN add profile filename=".\Prereqs\Wi-Fi-CAE Wifi.xml"
+Netsh WLAN connect name="CAE Wifi"
